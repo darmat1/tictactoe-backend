5b85a81f1c2a82897413818755e54e4d8f6a6aea
Andrew Kupriyanov <andrew.darmat@gmail.com>
2025-12-14 22:10:41 +0200

    feat: register AppController, add a health check endpoint, and simplify the root path response.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GameGateway } from './game.gateway';
 import { GameService } from './game.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [AppController],
   providers: [GameGateway, GameService],
 })
 export class AppModule { }
